139b74fe87e3e854a4aaf1a8e7562469cc39cdf1
VIVEK KUMAR SINGH <53953099+viveknitrr2021@users.noreply.github.com>
2021-04-23 13:02:09 +0530

    Add files via upload

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+import java.sql.*;
+
+/**
+ *
+ * @author VIVEK KUMAR SINGH
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","admin");
+            return con;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
---
+++ src/librarymgmt/LibraryMgmt.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarymgmt;
+
+/**
+ *
+ * @author VIVEK KUMAR SINGH
+ */
+public class LibraryMgmt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
